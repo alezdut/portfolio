8ee7707a368338e4d96d6ef100a3ee0a7bc05383
alezdut <alejandrozdut@gmail.com>
2020-12-13 19:45:38 -0300

    profile

--- src/components/mainCard/Profile.jsx
+++ src/components/mainCard/Profile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Profile.css";
 import { makeStyles } from '@material-ui/core/styles';
-import perfil from "../../resources/images/TPRS7H4PN-U015Z6CG1AS-ea48690b9144-512.png"
+import perfil from "../../resources/images/TPRS7H4PN-U015Z6CG1AS-ea48690b9144-512.png";
+import PublicIcon from '@material-ui/icons/Public';
 import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,8 +13,8 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     small: {
-        width: theme.spacing(3),
-        height: theme.spacing(3),
+        width: theme.spacing(2),
+        height: theme.spacing(2),
     },
     large: {
         width: theme.spacing(7),
@@ -27,6 +28,15 @@ export default function Profile() {
     return (
         <div className="main2__profile">
             <Avatar alt="Alejandro Zdut" src={perfil} className={classes.large} />
+            <div className="text__profile">
+                <p className="bold__profile">Alejandro Eloy Zdut</p>
+                <p className="sub__profile">Fullstack developer | React |
+Javascript | SQl | MongoDB | HTML | CSS</p>
+                <div className="icon__profile">
+                    <PublicIcon className={classes.small} />
+                    <p className="pub__profile">publico</p>
+                </div>
+            </div>
         </div>
     )
 }
