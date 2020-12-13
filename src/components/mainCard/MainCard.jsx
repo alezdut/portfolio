88c48a444b526097e5cf1850b7238733be2f5c0c
alezdut <alejandrozdut@gmail.com>
2020-12-13 19:42:47 -0300

    maincard

--- src/components/mainCard/MainCard.jsx
+++ src/components/mainCard/MainCard.jsx
@@ -10,7 +10,15 @@ export default function MainCard() {
                 <p className="head_title__maincard">Mi mama</p><p className="sub__head">recomienda esto</p>
             </div>
             <Divider />
-            <Profile />
+            <Profile className="profile__maincard" />
+            <div className="content__maincard">
+                <p>Hola! bienvenido a mi Portfolio, aqui podras ver algunos de mis trabajos y proyectos personales.<br /><br />
+                ESTO NO ES LINKEDIN! no te confundas! arme esta pagina desde 0 en react, los estilos son mayormente en CSS pero tambien
+                use Material UI. si te interesa podes ver el codigo en mi github <a href="https://github.com/alezdut">https://github.com/alezdut</a> junto con el
+                codigo de mis otros proyectos.<br /><br />
+                Gracias por tu visita!
+                </p>
+            </div>
         </div>
     )
 }
